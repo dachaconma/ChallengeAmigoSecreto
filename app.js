df0c34161e8be06e2ae15e98ceef5479d116bfd7
Diego Prueba <diegchacon_22@hotmail.com>
2025-02-04 22:32:12 -0500

    Se crean variables

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el proble
+//Se crea variables y crea función para leer cualquier cantidad de nombres
 
+let amigostot = [];
 
+function agregarAmigo() {
+    //Almacena lo que corresponde del campo id ="amigo"
+    const inputAmigo = document.getElementById('amigo');
+    const nombreAmigo = inputAmigo.value.trim();
+}
 
 
