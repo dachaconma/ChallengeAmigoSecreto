c6225c9b7c5af34ea57b1ae4fec5c23530da5cdb
Diego Prueba <diegchacon_22@hotmail.com>
2025-02-04 22:34:40 -0500

    Validar ingreso del campo

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ function agregarAmigo() {
     //Almacena lo que corresponde del campo id ="amigo"
     const inputAmigo = document.getElementById('amigo');
     const nombreAmigo = inputAmigo.value.trim();
+ 
+// Valida que el valor del campo sea solo letra
+const regex = /^[A-Za-záéíóúÁÉÍÓÚñÑ\s]+$/; // Expresión regular para letras y espacios
+if (nombreAmigo === '' || !regex.test(nombreAmigo)) {
+    alert('Por favor, ingresa un nombre válido (solo letras).');
+    return;
 }
 
-
+}
